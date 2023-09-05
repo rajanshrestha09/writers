61101d5ee1d01ea34f09a2742aeeac977263149e
Rajan Shrestha <stharaz09@gmail.com>
2023-09-05 15:38:27 -0600

    Add bootstrap

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 const Home = () => {
   return (
-    <div>Hello world</div>
+    <div className='h6 border border-dark border-2'> Hello world</div>
   )
 }
 
