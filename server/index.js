bcfef7bd77719c62495b61e7c0e95fee223d7377
Rajan Shrestha <stharaz09@gmail.com>
2023-09-05 22:05:52 -0600

    Setup route folder and page

---
+++ server/index.js
@@ -0,0 +1,22 @@
+//jshint esversion: 6
+import express from 'express'
+import 'dotenv/config'
+import routes from "./routes/route.js"
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+
+// Route to route.js
+app.use("/", routes);
+app.use('/signup', routes)
+app.use('/login', routes)
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server started at port number ${process.env.PORT}`)
+})
