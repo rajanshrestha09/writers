329ccc9620360755018926f3ffacc125b0abcce8
Rajan Shrestha <stharaz09@gmail.com>
2023-09-06 09:30:54 -0600

    Created new user and saved that data to mongodb database

--- server/routes/route.js
+++ server/routes/route.js
@@ -5,27 +5,62 @@ import User from './model/userModel.js'
 const router = express.Router();
 
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-router.post('/signup', (req,res)=>{
-    const {firstname, lastname, email, username, password} = req.body;
-    // console.log(req.body)ß
-    console.log(firstname)
+router.post('/signup', async (req, res) => {
+    try {
+        const { firstname, lastname, email, username, password } = await req.body;
+        console.log("Print Me: ", req.body)
+        const user_name = await User.findOne({ username }); // pull username from mongdb cluster
+        if (user_name) { // Check if user exists or not
+            return res.json({
+                error: "User already exist",
+                status: 400
+            })
+        }
 
-    res.send({
-        message: "success"
-    })
+        //Create new user
+        const newUser = new User(
+            {
+                firstname,
+                lastname,
+                email,
+                username,
+                password
+            }
+        )
+
+        const savedUser = await newUser.save();
+        console.log(savedUser)
+        
+        return res.json(
+            {
+                message: 'User created successfully',
+                success: true,
+                savedUser
+            }
+        )
+
+    } catch (error) {
+        return res.json(
+            {
+                error: error.message
+            },
+            {
+                status: 500
+            }
+        )
+    }
+
 })
 
-router.post('/login', (req,res)=>{
-    const {username, password} = req.body;
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
     console.log(username)
 
-    res.json({
-        message: "hello"
-    })
+
 })
 
 export default router;
