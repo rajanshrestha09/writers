ec20243886f1d4d635fa42466b2ce4cb9a27545a
Rajan Shrestha <stharaz09@gmail.com>
2023-09-05 22:05:12 -0600

    Added routes folder

---
+++ server/routes/route.js
@@ -0,0 +1,33 @@
+//jshint esversion: 6
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/', (req, res)=>{
+    res.send("Hello World")
+})
+
+router.post('/signup', (req,res)=>{
+    const {firstname, lastname, email, username, password} = req.body;
+    // console.log(req.body)ß
+    console.log(firstname)
+
+    res.send({
+        message: "success"
+    })
+})
+
+router.post('/login', (req,res)=>{
+    const {username, password} = req.body;
+    console.log(username)
+
+    res.json({
+        message: "hello"
+    })
+})
+
+
+
+
+export default router;
