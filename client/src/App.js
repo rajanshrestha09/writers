33d8b6924bb83c8c4dd016043146394fd65cb96f
Rajan Shrestha <stharaz09@gmail.com>
2023-09-06 21:14:47 -0600

    Necessary update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home"
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
   </BrowserRouter>
   );
 }
