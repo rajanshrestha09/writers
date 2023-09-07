aaf131dc27d8eda75016790d0766f351fe117baa
Rajan Shrestha <stharaz09@gmail.com>
2023-09-06 18:02:08 -0600

    Created token and cookies send to postman

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,8 @@
 //jshint esversion: 6
 import express from 'express';
-import User from './model/userModel.js'
-import  bcrypt from 'bcryptjs';
+import User from '../model/userModel.js'
+import bcrypt from 'bcryptjs';
+import token from '../tokenCreate/token.js';
 
 const router = express.Router();
 
@@ -62,9 +63,37 @@ router.post('/signup', async (req, res) => {
 
 })
 
-router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    console.log(username)
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log(username)
+        // Check if user already exists
+        const user_name = await User.findOne({ username });
+
+        if (!user_name) {
+            return res.json({
+                error: 'User not exists',
+                status: 400
+            })
+        }
+        // Check password
+        const validPassword = await bcrypt.compare(password, user_name.password)
+        if (!validPassword) {
+            return res.json({
+                error: 'Invalid Password',
+                status: 400
+            })
+        }
+        // Call token function
+        token(user_name, res)
+
+    } catch (error) {
+        return res.json({
+            error: error.message,
+            status: 500
+        })
+    }
+
 
 
 })
