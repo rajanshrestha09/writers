5588f41c0f75c30ad2a5dcb7cae36414ef4d7298
Rajan Shrestha <stharaz09@gmail.com>
2023-09-07 14:37:34 -0600

    Created login and profile page

---
+++ client/src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios'
+import Navbar from './Navbar';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    const navigate = useNavigate()
+    const [login, setLogin] = useState({
+        username: "",
+        password: ""
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        console.log(value)
+        setLogin(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        })
+    }
+
+    async function onsubmit() {
+        console.log('Here')
+        const response = await axios.post('http://localhost:8080/login', login,{headers: {'content-type': 'application/x-www-form-urlencoded'}});
+        console.log(response.data)
+        navigate('/profile')
+
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+                    <div className=" rounded rounded-5 p-5 bg-primary">
+                        <h1 className="text-center mb-4 text-light">Log In</h1>
+                        <form className="d-flex flex-column" onSubmit={onsubmit}>
+                            <div className="row g-3 align-self-end mt-1">
+                                <div className="col-auto">
+                                    <label forname="username" className="col-form-label text-light">Username</label>
+                                </div>
+                                <div className="col-auto">
+                                    <input type="text" name="username" id="username" value={login.username} onChange={handleChange} className="form-control" aria-describedby="passwordHelpInline" />
+                                </div>
+                            </div>
+                            <div className="row g-3 align-self-end mt-1">
+                                <div className="col-auto">
+                                    <label forname="password" className="col-form-label text-light">Password</label>
+                                </div>
+                                <div className="col-auto">
+                                    <input type="password" name="password" id="password" value={login.password} onChange={handleChange} className="form-control" aria-describedby="passwordHelpInline" />
+                                </div>
+                            </div>
+                            <div className="row g-3 align-self-center mt-1 ">
+                                <button type="submit" className="btn btn-info text-dark">Login</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Login;
---
+++ client/src/components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
