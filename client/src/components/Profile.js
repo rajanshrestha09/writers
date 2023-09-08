f7b5eee6171f474c0a54d9bfb20bf7315b7d908d
Rajan Shrestha <stharaz09@gmail.com>
2023-09-08 15:45:12 -0600

    Profile setup

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom';
+
 
 const Profile = () => {
-  axios.get('http://localhost:8080/signup');
+  const navigate = useNavigate()
+  useEffect(() => {
+    const checked = async () => {
+        const response = await axios.get('http://localhost:8080/profile', { headers: { 'content-type': 'application/json' }, withCredentials: true })
+        console.log(response.data)
+        if (response.data.status === 200) {
+            navigate('/profile')
+        }
+        if (response.data.status === 400) {
+            navigate('/login')
+        }
+    }
+    checked()
+}, [])
+
   return (
     <div>Profile</div>
   )
