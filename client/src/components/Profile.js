4d9e25c9541e33f328b12a7c511137fab45f3bae
Rajan Shrestha <stharaz09@gmail.com>
2023-09-08 16:47:42 -0600

    Pull details from api and display it on browser

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,27 +1,49 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const Profile = () => {
   const navigate = useNavigate()
+  const [user, setUser] = useState({
+    firstname: '',
+    lastname: '',
+    email: ''
+  })
+  console.log(user)
   useEffect(() => {
     const checked = async () => {
-        const response = await axios.get('http://localhost:8080/profile', { headers: { 'content-type': 'application/json' }, withCredentials: true })
-        console.log(response.data)
-        if (response.data.status === 200) {
-            navigate('/profile')
-        }
-        if (response.data.status === 400) {
-            navigate('/login')
-        }
+      const response = await axios.get('http://localhost:8080/profile', { headers: { 'content-type': 'application/json' }, withCredentials: true })
+      // console.log(response.data)
+      if (response.data.status === 200) {
+        const user_details = response.data.user
+        // console.log(user_details)
+        setUser({
+          firstname: user_details.firstname,
+          lastname: user_details.lastname,
+          email: user_details.email
+        })
+
+        navigate('/profile')
+      }
+      if (response.data.status === 400) {
+        navigate('/login')
+      }
     }
     checked()
-}, [])
+  }, [])
 
   return (
-    <div>Profile</div>
+    <>
+      <div>Profile</div>
+      <h1>{user.firstname}</h1>
+      <h1>{user.lastname}</h1>
+      <h1>{user.email}</h1>
+      
+    </>
+
+
   )
 }
 
