28013a25383a5904500ad2178a8823627aeb7ddc
Rajan Shrestha <stharaz09@gmail.com>
2023-09-07 20:15:47 -0600

    class change to className in Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 function Navbar() {
     return (
         <div className="bg-primary">
-            <h1 class="text-light fs-3 p-2 fst-italic">writers</h1>
+            <h1 className="text-light fs-3 p-2 fst-italic">writers</h1>
         </div>
     )
 }
