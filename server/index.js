3c2540f3006fcc2c484ffaf9773d472a84cd64a5
Rajan Shrestha <stharaz09@gmail.com>
2023-09-08 15:28:20 -0600

    route make call to controller

--- server/index.js
+++ server/index.js
@@ -21,67 +21,6 @@ app.use(cors({
 connect();//Database connection
 
 
-
-
-// Route to route.js
-// app.use((req,res,next)=>{
-//     const path = req.url
-//     const token = req.cookies.token
-//     console.log("Next console:", path, token)
-//     const publicPath = '/login' || '/signup';  
-
-//     if(token){
-//         console.log('have value') 
-
-//     }else{
-//         console.log('have no value') 
-
-//     } 
-//     next()
-// })
-
-app.get('/login', (req, res) => {
-    const path = req.url
-    const token = req.cookies.token
-    console.log("Next console:", path, token)
-    const publicPath = path === '/login' || '/signup';
-    if (publicPath && token) {
-        res.json({
-            message: "has token",
-            status: 200
-        })
-    }
-    if (publicPath && !token) {
-        res.json({
-            message: "has no token",
-            status: 400
-        })
-    }
-
-
-})
-
-app.get('/signup', (req, res) => {
-    const path = req.url
-    const token = req.cookies.token
-    console.log("Next console:", path, token)
-    const publicPath = path === '/login' || '/signup';
-    if (publicPath && token) {
-        res.json({
-            message: "has token",
-            status: 200
-        })
-    }
-
-    if (publicPath && !token) {
-        res.json({
-            message: "has no token",
-            status: 400
-        })
-    }
-
-
-})
 app.use("/", routes);
 app.use('/signup', routes)
 app.use('/login', routes)
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
 import express from 'express';
-import User from '../model/userModel.js'
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import signupPost from './controller/signuppost.js';
+import getReuest from './controller/getRequest.js';
+import login from './controller/login.js';
+const app = express();
 
 const router = express.Router();
 
@@ -10,109 +11,10 @@ router.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-router.post('/signup', async (req, res) => {
-    try {
-        const { fname, lname, email, username, password } = await req.body;
-        console.log("Print Me: ", req.body)
-        const user_name = await User.findOne({ username }); // pull username from mongdb cluster
-        if (user_name) { // Check if user exists or not
-            return res.json({
-                error: "User already exist",
-                status: 400
-            })
-        }
-        // Hash a password
-        console.log('1')
-        const salt = bcrypt.genSaltSync(10);
-        const hashPassword = await bcrypt.hash(password, salt) // Store hash in your password DB.
-        console.log('2')
-        //Create new user
-        const newUser = new User(
-            {
-                firstname: fname,
-                lastname: lname,
-                email: email,
-                username: username,
-                password: hashPassword
-            }
-        )
-        console.log('3')
-        const savedUser = await newUser.save();
-        console.log(savedUser)
-        return res.json(
-            {
-                message: 'User created successfully',
-                success: true,
-                savedUser
-            }
-        )
-    } catch (error) {
-        return res.json(
-            {
-                error: error.message
-            },
-            {
-                status: 500
-            }
-        )
-    }
+router.get('/login', getReuest)
+router.get('/signup', getReuest)
+router.post('/signup', signupPost)
+router.post('/login', login)
 
-})
-router.post('/login', async (req, res) => {
-    try {
-        const { username, password } = await req.body;
-        console.log(username)
-        // Check if user already exists
-        const user_name = await User.findOne({ username });
-        console.log(user_name)
-        //////////
-        if (!user_name) {
-            return res.json({
-                error: 'User not exists',
-                status: 400
-            })
-        }
-        // Check password
-        const validPassword = await bcrypt.compare(password, user_name.password)
-        if (!validPassword) {
-            return res.json({
-                error: 'Invalid Password',
-                status: 400
-            })
-        }
-        console.log('validpassword')
-        // token function
-        const tokenData = {
-            id: user_name.id,
-            username: user_name.username,
-            email: user_name.email
-        }
-        console.log(tokenData)
-        // create token
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {
-            expiresIn: "1h"
-        })
-        console.log(token)
-        res.header('Content-Type', 'application/json;charset=UTF-8')
-        res.header('Access-Control-Allow-Credentials', true)
-        res.header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept'
-        )
-
-        res.cookie('token', token, {
-            httpOnly: true
-        })
-        return res.json({
-            message: "success",
-            status: 200
-        });
-    } catch (error) {
-        return res.json({
-            error: error.message,
-            status: 500
-        })
-    }
-})
 
 export default router;
