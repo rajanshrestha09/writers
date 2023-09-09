8936b166fc78036bd26367f7bd0e9ceeb14d93a5
Rajan Shrestha <rajan.shr09@gmail.com>
2023-09-09 11:23:26 -0600

    Added vercel.json

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(express.json())
 // app.use(bodyParser.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
+    methods:["POST", "GET"],
     credentials: true
 })
 )
